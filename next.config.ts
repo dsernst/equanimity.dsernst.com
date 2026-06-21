14945086b1fd5957eb1e0d36ce5726678425d36a
David Ernst <git@dsernst.com>
2026-06-21 14:54:56 -0700

    nr dev:https script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-};
+  allowedDevOrigins: ['192.168.4.22'],
+}
 
-export default nextConfig;
+export default nextConfig

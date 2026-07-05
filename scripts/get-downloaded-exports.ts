13be831e4b333246bc796059ab33881bf34f2e32
David Ernst <git@dsernst.com>
2026-07-04 20:06:19 -0700

    Add script to grab downloaded exports

---
+++ scripts/get-downloaded-exports.ts
@@ -0,0 +1,23 @@
+import { renameSync, existsSync, readdirSync } from 'node:fs'
+import { join, basename } from 'node:path'
+import { homedir } from 'node:os'
+
+const EXPORTS_REGEX = /^\d{4}-\d{2}-\d{2}T\d{2}_\d{2}_\d{2}\.tsv$/ // eg `2026-06-19T17_07_29.tsv`
+const DownloadsDir = join(homedir(), 'Downloads')
+const LogsDir = join(import.meta.dir, '..', 'logs')
+
+// Get list of export TSVs in ~/Downloads
+const tsvs = readdirSync(DownloadsDir)
+  .filter((name) => EXPORTS_REGEX.test(name))
+  .map((name) => join(DownloadsDir, name))
+
+// Move from ~/Downloads to ./logs
+for (const src of tsvs) {
+  const dest = join(LogsDir, basename(src))
+  if (existsSync(dest)) continue
+  renameSync(src, dest)
+  console.log(`moved ${basename(src)}`)
+}
+
+// Print if none
+if (tsvs.length === 0) console.log('no new exports in ~/Downloads')
